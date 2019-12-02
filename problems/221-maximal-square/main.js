e6745cb1448a3c95a79e4cebec014690025ee3a5
Siddhartha <sidduinevitable@gmail.com>
2019-12-02 14:24:17 +0530

    js solution for maximal square

---
+++ problems/221-maximal-square/main.js
@@ -0,0 +1,12 @@
+const maximalSquare = function (matrix) {
+  let count = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] > 0 && i > 0 && j > 0) {
+        matrix[i][j] = Math.min(matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i][j - 1]) + 1;
+      }
+      count = count > matrix[i][j] ? count : matrix[i][j];
+    }
+  }
+  return count * count;
+};
