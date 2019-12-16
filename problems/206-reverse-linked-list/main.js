d9334511d3e79a11181c93032982de9e5c857096
Siddhartha <sidduinevitable@gmail.com>
2019-12-16 20:11:33 +0530

    js solution for the reverse linked list

---
+++ problems/206-reverse-linked-list/main.js
@@ -0,0 +1,10 @@
+const reverseList = function(head) {
+  let prev = null;
+  while (head) {
+    let temp = head.next;
+    head.next = prev;
+    prev = head;
+    head = temp;
+  }
+  return prev;
+};
