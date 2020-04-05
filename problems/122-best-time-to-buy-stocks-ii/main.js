aa2e82df171277808d1ec16d374fc2d1ba569567
Siddhartha <sidduinevitable@gmail.com>
2020-04-05 16:26:34 +0530

    js solution for best time to buy the stocks

---
+++ problems/122-best-time-to-buy-stocks-ii/main.js
@@ -0,0 +1,12 @@
+const maxProfit = function(prices) {
+  if (!prices.length) {
+    return 0;
+  }
+  let profit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (prices[i + 1] > prices[i]) {
+      profit += prices[i + 1] - prices[i];
+    }
+  }
+  return profit;
+};
