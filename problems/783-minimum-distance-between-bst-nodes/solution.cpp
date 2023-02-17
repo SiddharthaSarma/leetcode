cdc409de6060c189f9b8fa700f0e566647051aab
Siddhartha <sidduinevitable@gmail.com>
2023-02-17 21:23:13 +0530

    solution for 783. Minimum distance between bst nodes

---
+++ problems/783-minimum-distance-between-bst-nodes/solution.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/minimum-distance-between-bst-nodes/description/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  public: vector < int > list = {};
+  void inorderTraversal(TreeNode * root) {
+    if (!root) return;
+    inorderTraversal(root -> left);
+    list.push_back(root -> val);
+    inorderTraversal(root -> right);
+  }
+  int minDiffInBST(TreeNode * root) {
+    inorderTraversal(root);
+    int minDistance = INT_MAX;
+    for (int i = 0; i < list.size() - 1; i++) {
+      minDistance = min(minDistance, abs(list[i] - list[i + 1]));
+    }
+    return minDistance;
+  }
+};
